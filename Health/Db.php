ccfb9894cf6e24e0d0d004ad26a2a7314d6a06bd
Cully Larson <cully.larson@gmail.com>
2018-10-11 01:05:14 -0700

    Added the report page.

--- Health/Db.php
+++ Health/Db.php
@@ -46,4 +46,17 @@ class Db {
             $this->dbh->rollback();
         }
     }
+
+    public function getResults($siteId, $limit = 10) {
+        $limit = (int) $limit;
+
+        $sth = $this->dbh
+            ->prepare("SELECT * from results WHERE siteId = :siteId ORDER BY created DESC LIMIT {$limit}");
+
+        $sth->execute([
+            ':siteId' => $siteId,
+        ]);
+
+        return $sth->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,201 @@
 <?php
 
 require __DIR__ . '/autoload.php';
+
+use Health\Db;
+use function Phugly\call;
+use function Phugly\compose;
+use function Phugly\curry;
+use function Phugly\map;
+use function Phugly\glue;
+use function Phugly\setAt;
+
+$db = new Db(getenv('DB_DSN'), getenv('DB_USER'), getenv('DB_PASS'));
+
+function esc($x) {
+    return htmlspecialchars($x);
+}
+
+$renderSite = function($site) {
+    $renderResult = function($result) {
+        $className = $result['isUp'] ? 'is-up' : 'not-up';
+
+        ob_start();
+        ?>
+        <div class='result <?= $className; ?>'>
+            <div class='result-text'><?= esc($result['created']); ?></div>
+        </div>
+        <?php
+
+        return ob_get_clean();
+    };
+
+    ob_start();
+    ?>
+    <div class='site'>
+        <div class='name'><a href='<?= $site['url']; ?>'><?= esc($site['name']); ?></a></div>
+        <div class='last-checked'><?= esc($site['lastChecked']); ?></div>
+        <div class='results'>
+            <?= call(compose(
+                glue(''),
+                map($renderResult)
+            ), $site['results']); ?>
+        </div>
+    </div>
+    <?php
+
+    return ob_get_clean();
+};
+
+$augResults = curry(function($db, $site) {
+    return setAt('results', $db->getResults($site['id'], 20), $site);
+});
+
+$sites = call(compose(
+    map($augResults($db))
+), $db->getAllSites());
+
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>Health Check</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto|Vollkorn" rel="stylesheet">
+
+
+        <link rel='icon' type='image/png' sizes='256x256' href='./favicon-256.png' />
+        <link rel='icon' type='image/png' sizes='1024x1024' href='./favicon-1024.png' />
+
+        <style>
+            html {
+                box-sizing: border-box;
+            }
+
+            *, :after, :before {
+                box-sizing: inherit;
+            }
+
+            body {
+                margin: 0;
+                padding: 40px;
+                font-family: 'Roboto', sans-serif;
+                background: #f3f5f7;
+            }
+
+            h1,
+			h2,
+			h3,
+			h4 {
+                font-family: 'Vollkorn', serif;
+            }
+
+            a,
+            a:active,
+            a:visited {
+                text-decoration: none;
+                color: #005eb3;
+            }
+
+            a:hover {
+                opacity: 0.8;
+            }
+
+            #content {
+                width: 900px;
+                max-width: 100%;
+                margin: 0 auto;
+                padding: 40px 60px;
+                background: white;
+            }
+
+            #content > h1 {
+                margin-top: 0;
+            }
+
+            .site {
+                display: flex;
+                align-items: center;
+                margin-bottom: 20px;
+            }
+
+            .last-checked {
+                padding-left: 15px;
+                color: #555;
+                font-size: 14px;
+            }
+
+            .results {
+                display: flex;
+                padding-left: 15px;
+            }
+
+            .results .result {
+                width: 10px;
+                height: 10px;
+                margin-right: 5px;
+                border-radius: 10px;
+                position: relative
+            }
+
+            .results .result.is-up {
+                background: #5bb36f;
+            }
+
+            .results .result.not-up {
+                background: #f25255;
+            }
+
+            .results .result .result-text {
+                display: none;
+                position: absolute;
+                top: -28px;
+                left: -12px;
+                background: #ddd;
+                color: #857587;
+                padding: 3px;
+                border-radius: 3px;
+                font-size: 12px;
+                z-index: 1;
+                min-width: 125px;
+                text-align: center;
+            }
+
+            .results .result .result-text:after {
+                content: '';
+                display: block;
+                position: absolute;
+                left: 12px;
+                bottom: -3px;
+                background: #ddd;
+                width: 10px;
+                height: 10px;
+                transform: rotate(45deg);
+                z-index: -1;
+            }
+
+            .results .result:hover .result-text {
+                display: block;
+            }
+        </style>
+
+        <script>
+            setTimeout(function() {
+               window.location.reload(1)
+            }, 310000)
+        </script>
+    </head>
+    <body>
+        <div id="content">
+            <h1>Health Check</h1>
+
+            <?= call(compose(
+                glue(''),
+                map($renderSite)
+            ), $sites); ?>
+        </div>
+    </body>
+</html>
