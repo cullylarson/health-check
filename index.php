a3b8d5ba4ac850feb5d4bf3603e569f8a5b9f1d3
Cully Larson <cully.larson@gmail.com>
2018-10-15 23:40:40 -0700

    Added charts. Still need to tweak the design a bit but I need to sleep.

--- index.php
+++ index.php
@@ -10,12 +10,22 @@ use function Phugly\map;
 use function Phugly\glue;
 use function Phugly\getAt;
 use function Phugly\setAt;
+use function Phugly\firstN;
 
 function esc($x) {
     return htmlspecialchars($x);
 }
 
-$renderSite = function($site) {
+function randStr($length=20) {
+    $characters = "abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    $str = "";
+    for($i=0; $i < $length; $i++) $str .= substr($characters, rand(0, strlen($characters) - 1), 1);
+    return $str;
+}
+
+
+$renderSite = curry(function($numResults, $site) {
     $renderResult = function($result, $isLastDown = false) {
         if(empty($result)) return '';
 
@@ -39,23 +49,100 @@ $renderSite = function($site) {
         return ob_get_clean();
     };
 
+    $renderChart = function($site) {
+        $chartId = 'chart-' . randStr(12);
+
+        $labelsJson = call(compose(
+            'json_encode',
+            map(function($x) { return ''; }),
+            map(getAt('created', ''))
+        ), $site['results']);
+
+        $responseTimesJson = call(compose(
+            'json_encode',
+            map(function($x) {
+                return [
+                    'value' => getAt('responseTime', 0, $x),
+                    'meta' => getAt('created', '', $x) . ' | ' . getAt('responseTime', 0, $x),
+                ];
+            })
+        ), $site['results']);
+
+        ob_start();
+        ?>
+        <div class='chart' id='<?= $chartId; ?>'>
+            <div class='chart-toggle'>Chart</div>
+            <div class='chart-container'></div>
+        </div>
+        <script>
+            (() => {
+                const chartEl = document.querySelector('#<?= $chartId; ?>')
+                const toggleEl = chartEl.querySelector('.chart-toggle')
+                toggleEl.addEventListener('click', () => chartEl.classList.toggle('is-open'))
+            })()
+
+            new Chartist.Line('#<?= $chartId; ?> .chart-container', {
+                    // labels: <?= $labelsJson; ?>,
+                    series: [<?= $responseTimesJson; ?>],
+                }, {
+                    fullWidth: true,
+                    height: '200px',
+                },
+            )
+                .on('created', () => {
+                    document.querySelectorAll('#<?= $chartId; ?> .ct-point').forEach(x => {
+                        let hoverEl
+
+                        x.addEventListener('mouseover', e => {
+                            const el = e.currentTarget
+                                console.log(el.clientX, el.clientY)
+                            el.classList.add('hovered')
+
+                            const elBounds = el.getBoundingClientRect()
+
+                            const meta = el.getAttribute('ct:meta')
+
+                            hoverEl = document.createElement('div')
+                            hoverEl.classList.add('chart-tooltip')
+                            hoverEl.innerHTML = meta
+                            hoverEl.style.left = (elBounds.left - 10) + 'px'
+                            hoverEl.style.top = (elBounds.top - 35) + 'px'
+                            document.body.appendChild(hoverEl)
+                        })
+
+                        x.addEventListener('mouseout', e => {
+                            const el = e.currentTarget
+                            el.classList.remove('hovered')
+                            if(hoverEl) hoverEl.remove()
+                        })
+                    })
+                })
+        </script>
+        <?php
+        return ob_get_clean();
+    };
+
     ob_start();
     ?>
     <div class='site'>
-        <div class='name'><a href='<?= $site['url']; ?>'><?= esc($site['name']); ?></a></div>
-        <div class='last-checked'><?= esc($site['lastChecked']); ?></div>
-        <div class='results'>
-            <?= call(compose(
-                glue(''),
-                map($renderResult)
-            ), $site['results']); ?>
-            <?= $renderResult($site['lastDownResult'], true); ?>
+        <div class='site-summary'>
+            <div class='name'><a href='<?= $site['url']; ?>'><?= esc($site['name']); ?></a></div>
+            <div class='last-checked'><?= esc($site['lastChecked']); ?></div>
+            <div class='results'>
+                <?= call(compose(
+                    glue(''),
+                    map($renderResult),
+                    firstN($numResults)
+                ), $site['results']); ?>
+                <?= $renderResult($site['lastDownResult'], true); ?>
+            </div>
         </div>
+        <?= $renderChart($site); ?>
     </div>
     <?php
 
     return ob_get_clean();
-};
+});
 
 $augResults = curry(function($db, $numResults, $site) {
     return setAt('results', $db->getResults($site['id'], $numResults), $site);
@@ -67,9 +154,12 @@ $augLastDown = curry(function($db, $site) {
 
 $db = new Db(getenv('DB_DSN'), getenv('DB_USER'), getenv('DB_PASS'));
 
+$numResults = getAt('NUM_RESULTS', 20, $_ENV);
+$numGraphResults = getAt('NUM_GRAPH_RESULTS', 100, $_ENV);
+
 $sites = call(compose(
     map($augLastDown($db)),
-    map($augResults($db, getAt('NUM_RESULTS', 20, $_ENV)))
+    map($augResults($db, $numGraphResults))
 ), $db->getAllSites());
 
 $xSmall = '@media (max-width: 450px)';
@@ -86,6 +176,8 @@ $small = '@media (max-width: 650px)';
 
         <link href="https://fonts.googleapis.com/css?family=Roboto|Vollkorn" rel="stylesheet">
 
+        <link href="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
 
         <link rel='icon' type='image/png' sizes='256x256' href='./favicon-256.png' />
         <link rel='icon' type='image/png' sizes='1024x1024' href='./favicon-1024.png' />
@@ -161,15 +253,23 @@ $small = '@media (max-width: 650px)';
             }
 
             .site {
-                display: flex;
-                align-items: center;
                 margin-bottom: 20px;
             }
 
             <?= $small; ?> {
                 .site {
-                    display: block;
                     margin-bottom: 40px;
+                }
+            }
+
+            .site-summary {
+                display: flex;
+                align-items: center;
+            }
+
+            <?= $small; ?> {
+                .site-summary {
+                    display: block;
                 }
             }
 
@@ -236,12 +336,11 @@ $small = '@media (max-width: 650px)';
                 left: -12px;
                 background: #8F7A6A;
                 color: #E1DFDF;
-                padding: 3px;
+                padding: 3px 5px;
                 border-radius: 3px;
                 font-size: 12px;
                 z-index: 1;
-                min-width: 190px;
-                text-align: center;
+                white-space: nowrap;
             }
 
             .results .result .result-text:after {
@@ -260,6 +359,80 @@ $small = '@media (max-width: 650px)';
             .results .result:hover .result-text {
                 display: block;
             }
+
+            .chart-toggle {
+                display: inline-block;
+                cursor: pointer;
+                user-select: none;
+                margin-top: 5px;
+                background: #DBC07B;
+                color: #555;
+                border-radius: 3px;
+                padding: 3px 5px;
+                font-size: 12px;
+            }
+
+            .chart .chart-container {
+                transition: max-height 300ms ease;
+                max-height: 0;
+                overflow: hidden;
+                padding-top: 10px;
+            }
+
+            .chart.is-open .chart-container {
+                max-height: 200px;
+            }
+
+            .chart-toggle {
+                
+            }
+
+            .ct-grid {
+                stroke: #444;
+            }
+
+            .ct-label {
+                color: #777;
+            }
+
+            .ct-series-a .ct-line,
+            .ct-series-a .ct-point {
+                stroke: #E33051;
+            }
+
+            .ct-series-a .ct-point {
+                stroke-width: 8px;
+            }
+
+            .ct-series-a .ct-line {
+                stroke-width: 1px;
+            }
+
+            .chart-tooltip {
+                position: fixed;
+                z-index: 2;
+                background: #8F7A6A;
+                color: #E1DFDF;
+                padding: 3px;
+                border-radius: 3px;
+                font-size: 12px;
+                z-index: 1;
+                padding: 3px 5px;
+                white-space: nowrap;
+            }
+
+            .chart-tooltip:after {
+                content: '';
+                display: block;
+                position: absolute;
+                left: 5px;
+                bottom: -3px;
+                background: #8F7A6A;
+                width: 10px;
+                height: 10px;
+                transform: rotate(45deg);
+                z-index: -1;
+            }
         </style>
 
         <script>
@@ -274,7 +447,7 @@ $small = '@media (max-width: 650px)';
 
             <?= call(compose(
                 glue(''),
-                map($renderSite)
+                map($renderSite($numResults))
             ), $sites); ?>
         </div>
     </body>
